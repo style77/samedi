1da37b3d712f047ce5949603b7eaed5e2ccff0fa
style77 <stylek777@gmail.com>
2024-03-29 13:27:03 +0100

    chore: database schema

---
+++ internal/database/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS blogs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL,
+
+    name TEXT NOT NULL UNIQUE,
+    description TEXT NULL
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL,
+
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    blog_id INTEGER NOT NULL,
+    FOREIGN KEY(blog_id) REFERENCES blogs(id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_posts_created_at ON posts(created_at);
+CREATE INDEX IF NOT EXISTS idx_posts_updated_at ON posts(updated_at);
+CREATE INDEX IF NOT EXISTS idx_posts_title ON posts(title);
+CREATE INDEX IF NOT EXISTS idx_posts_blog_id ON posts(blog_id);
