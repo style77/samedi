bc6f8c61c34d7b8c1442bc03f142d42c60665d74
style77 <stylek777@gmail.com>
2024-03-29 13:25:10 +0100

    chore: retrieve blog helper

---
+++ internal/blogs/retrieve.go
@@ -0,0 +1,24 @@
+package blogs
+
+import (
+	"fmt"
+
+	"github.com/style77/samedi/internal/database"
+)
+
+func GetBlog(name string) (*Blog, error) {
+	database, err := database.Init()
+	if err != nil {
+		return nil, err
+	}
+
+	var blog Blog
+	err = database.QueryRow("SELECT id, name, description, created_at, updated_at FROM blogs WHERE name = ?", name).Scan(&blog.ID, &blog.Name, &blog.Description, &blog.CreatedAt, &blog.UpdatedAt)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("No blog found with name:", name)
+		return nil, err
+	}
+
+	return &blog, nil
+}
