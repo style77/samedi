534242e0aec600c037f0b1336b896a604c4e5806
style77 <stylek777@gmail.com>
2024-03-29 16:53:56 +0100

    chore: GetPosts function to retrieve multiple posts

--- internal/posts/retrieve.go
+++ internal/posts/retrieve.go
@@ -16,3 +16,28 @@ func GetPost(id int, blogId int) (*Post, error) {
 
 	return &post, nil
 }
+
+func GetPosts(blogId int) ([]Post, error) {
+	database, err := database.Init()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := database.Query("SELECT id, title, content, created_at, updated_at FROM posts WHERE blog_id = ?", blogId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var posts []Post
+	for rows.Next() {
+		var post Post
+		err := rows.Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt, &post.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
