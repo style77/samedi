d8fd0b771cb3b5c873f7fb482af67227cf27cf4c
style77 <stylek777@gmail.com>
2024-03-29 17:06:48 +0100

    chore: checkIfPortIsAvailable function to ensure port availability

--- internal/web/server.go
+++ internal/web/server.go
@@ -3,10 +3,20 @@ package web
 import (
 	"flag"
 	"fmt"
+	"net"
 
 	"github.com/style77/samedi/internal/blogs"
 	"github.com/style77/samedi/web"
 )
+
+func checkIfPortIsAvailable(host string, port int) bool {
+	conn, err := net.DialTCP("tcp", nil, &net.TCPAddr{IP: net.ParseIP(host), Port: port})
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
 
 func ServeCommand(args ...string) {
 	var host string
@@ -24,6 +34,14 @@ func ServeCommand(args ...string) {
 	blog, err := blogs.GetBlog(args[0])
 	if err != nil {
 		return
+	}
+
+	for checkIfPortIsAvailable(host, port) {
+		port++
+	}
+
+	if port != 5400 {
+		fmt.Println("Port 5400 is already in use, using port", port)
 	}
 
 	server := web.NewServer(host, port)
