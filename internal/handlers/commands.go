37747fe7700459a9597304cff9bfc083095ac5d7
style77 <stylek777@gmail.com>
2024-03-29 13:27:50 +0100

    feat: serve blog command

--- internal/handlers/commands.go
+++ internal/handlers/commands.go
@@ -1,16 +1,20 @@
 package handlers
 
 import (
+	"github.com/style77/samedi/internal/blogs"
 	"github.com/style77/samedi/internal/cli"
+	"github.com/style77/samedi/internal/web"
 )
 
 func Init() {
-	cli.RegisterCommand("new_post", NewPostCommand, []string{"newpost", "new", "post"}, []cli.CommandArgument{
-		{
-			Name:     "title",
-			Required: true,
-			Position: 0,
-		},
-	}, "Create a new post")
+	cli.RegisterCommand("create_blog", blogs.CreateBlogCommand, []string{"createblog"}, []cli.CommandArgument{
+		{Name: "name", Required: true, Position: 0},
+		{Name: "description", Required: false, Position: 1},
+	}, "Create a new blog")
+	cli.RegisterCommand("serve", web.ServeCommand, []string{}, []cli.CommandArgument{
+		{Name: "blog", Required: true, Position: 0},
+		{Name: "host", Required: false, Position: 1, IsFlag: true, Default: "localhost"},
+		{Name: "port", Required: false, Position: 2, IsFlag: true, Default: "8080"},
+	}, "Start the web server")
 	cli.RegisterCommand("help", HelpCommand, []string{}, []cli.CommandArgument{}, "Show this help message")
 }
