434efc5be7a9d4951a8991161d3a3983928ce20b
style77 <stylek777@gmail.com>
2024-03-29 15:09:14 +0100

    fix: retrieve post basing on blogId to prevent leaking posts from different blogs

--- internal/posts/retrieve.go
+++ internal/posts/retrieve.go
@@ -2,14 +2,14 @@ package posts
 
 import "github.com/style77/samedi/internal/database"
 
-func GetPost(id int) (*Post, error) {
+func GetPost(id int, blogId int) (*Post, error) {
 	database, err := database.Init()
 	if err != nil {
 		return nil, err
 	}
 
 	var post Post
-	err = database.QueryRow("SELECT id, title, content, created_at, updated_at, blog_id FROM posts WHERE id = ?", id).Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt, &post.UpdatedAt, &post.Blog.ID)
+	err = database.QueryRow("SELECT id, title, content, created_at, updated_at FROM posts WHERE id = ? AND blog_id = ?", id, blogId).Scan(&post.ID, &post.Title, &post.Content, &post.CreatedAt, &post.UpdatedAt, &post.Blog.ID)
 	if err != nil {
 		return nil, err
 	}
