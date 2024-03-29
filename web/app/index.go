9a1417b31245dbe3f6159cc3e7d8debc5ba24c4d
style77 <stylek777@gmail.com>
2024-03-29 16:54:57 +0100

    feat: index.go, index.css, and index.html files for the app

---
+++ web/app/index.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/style77/samedi/internal/blogs"
+	"github.com/style77/samedi/internal/posts"
+)
+
+//go:embed templates/*
+var templates embed.FS
+
+func IndexHandler(blog *blogs.Blog) http.HandlerFunc {
+	tmpl, err := template.ParseFS(templates, "templates/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		postsData, err := posts.GetPosts(blog.ID)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "Posts not found", http.StatusNotFound)
+			return
+		}
+
+		err = tmpl.Execute(w, struct {
+			Blog  *blogs.Blog
+			Posts []posts.Post
+		}{
+			Blog:  blog,
+			Posts: postsData,
+		})
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	}
+}
