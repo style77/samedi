91546b3fe14b7f2e02fcf28f74bf89854935cbc7
style77 <stylek777@gmail.com>
2024-03-29 16:54:13 +0100

    refactor: server port to use 5400 instead of 8080

--- internal/web/server.go
+++ internal/web/server.go
@@ -3,15 +3,10 @@ package web
 import (
 	"flag"
 	"fmt"
-	"net/http"
 
 	"github.com/style77/samedi/internal/blogs"
+	"github.com/style77/samedi/web"
 )
-
-type Server struct {
-	Host string
-	Port int
-}
 
 func ServeCommand(args ...string) {
 	var host string
@@ -19,7 +14,7 @@ func ServeCommand(args ...string) {
 
 	flags := flag.NewFlagSet("server", flag.ExitOnError)
 	flags.StringVar(&host, "host", "localhost", "host to serve on")
-	flags.IntVar(&port, "port", 8080, "port to serve on")
+	flags.IntVar(&port, "port", 5400, "port to serve on")
 	err := flags.Parse(args[1:])
 	if err != nil {
 		fmt.Println("Error parsing flags:", err)
@@ -31,32 +26,7 @@ func ServeCommand(args ...string) {
 		return
 	}
 
-	server := NewServer(host, port)
+	server := web.NewServer(host, port)
 
 	server.Serve(blog)
-}
-
-func NewServer(host string, port int) *Server {
-	return &Server{
-		Host: host,
-		Port: port,
-	}
-}
-
-func (s *Server) Serve(blog *blogs.Blog) {
-	handler := http.NewServeMux()
-	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
-
-	go func() {
-		err := http.ListenAndServe(fmt.Sprintf("%s:%d", s.Host, s.Port), handler)
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	fmt.Printf("Server listening on http://%s:%d\n", s.Host, s.Port)
-
-	select {}
 }
