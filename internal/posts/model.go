362030101b5ef4f73f122ef8726c4e66b67442fc
style77 <stylek777@gmail.com>
2024-03-29 16:53:42 +0100

    fix: change type of createdAt and updatedAt to  time.Time

--- internal/posts/model.go
+++ internal/posts/model.go
@@ -1,12 +1,16 @@
 package posts
 
-import "github.com/style77/samedi/internal/blogs"
+import (
+	"time"
+
+	"github.com/style77/samedi/internal/blogs"
+)
 
 type Post struct {
 	ID        int
 	Title     string
 	Content   string
-	CreatedAt string
-	UpdatedAt string
+	CreatedAt time.Time
+	UpdatedAt *time.Time
 	Blog      *blogs.Blog
 }
